61f69325d40709ba154c726142e00c41b0cec622
SahnouneAyaa <sahnouneaya15@gmail.com>
2026-06-17 13:30:58 +0100

    A fin Tech App

---
+++ next.config.mjs
@@ -0,0 +1,47 @@
+import {withSentryConfig} from '@sentry/nextjs';
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+
+export default withSentryConfig(nextConfig, {
+
+
+
+
+silent: true,
+org: "jsm-x9",
+project: "javascript-nextjs",
+}, {
+
+
+
+
+widenClientFileUpload: true,
+
+
+transpileClientSDK: true,
+
+
+
+
+
+
+
+
+hideSourceMaps: true,
+
+
+disableLogger: true,
+
+
+
+
+
+automaticVercelMonitors: true,
+});
